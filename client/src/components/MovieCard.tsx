0f47435a6b9770ad108586866f56a5b2df11f04f
Ryan Karumanchery <ryankarumanchery@gmail.com>
2022-11-14 23:44:00 -0500

    Updated movie card component - Props passed to component - Added blur to card - Added file for querying tmdb - Updated styles

--- client/src/components/MovieCard.tsx
+++ client/src/components/MovieCard.tsx
@@ -1,21 +1,27 @@
 import React from "react";
 import "../styles/movieCard.css";
 
-export default function MovieCard(
-  title: string,
-  img: string,
-  description: string,
-  genres: string[]
-) {
+interface MovieCardProps {
+  title: string;
+  img: string;
+  description: string;
+  genres: string[];
+}
+
+export default function MovieCard(props: MovieCardProps) {
   return (
     <div className="feature-movie-card">
-      <img src={img} alt="movie poster" />
+      <img src={props.img} alt="movie backdrop" />
       {/* <div className="colored" /> */}
       <div className="feature-movie-card-text">
-        <h2>{title}</h2>
-        <p>{description}</p>
+        <div className="title">{props.title}</div>
+        <p className="desc">{props.description}</p>
+        <div className="bottom-text">
+          <div className="genres">{props.genres}</div>
+          {/* //TODO: Map styles to above genres */}
+          <button className="details">Details</button>
+        </div>
       </div>
-      <div>{genres}</div>
     </div>
   );
 }
---
+++ server/src/tmdb.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+import * as dotenv from "dotenv";
+import * as fs from "fs";
+
+// import database connection
+const con = require("./mysql");
+
+dotenv.config();
