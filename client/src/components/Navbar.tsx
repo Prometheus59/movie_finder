ed23cc2a13da2d6f60df511344c9ce113eb5df7f
Ryan Karumanchery <ryankarumanchery@gmail.com>
2022-11-16 23:39:05 -0500

    Changed Navbar - Added styles - Changed order - Added icon

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,16 +1,42 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 import "../styles/navbar.css";
+
+const logo = require("../images/favicon.ico");
 
 function Navbar() {
   return (
     <div className="navbar">
-      <Link to="/">WatchIt</Link>
-      <h2>Search</h2>
+      <Link to="/">
+        <img id="logo" src={logo} alt="WatchIt Logo" />
+        <p>Watch It</p>
+      </Link>
       <Link to="/trending">Trending</Link>
+      {/* //TODO: Make above a dropdown? */}
 
-      <div>Toggle</div>
+      <nav>
+        <div className="media-types">
+          <NavLink
+            to="/movies"
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+          >
+            Movies
+          </NavLink>
+          <NavLink
+            to="/tv"
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+          >
+            TV
+          </NavLink>
+          <NavLink
+            to="/any"
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+          >
+            Any
+          </NavLink>
+        </div>
+      </nav>
 
       <div>
         <h3>Settings</h3>
