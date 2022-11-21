0aa4dc5aab253125ca8fd11c19b175bb72183774
Ryan Karumanchery <ryankarumanchery@gmail.com>
2022-11-21 12:36:36 -0500

    Restructure MovieCard - General movieCard template to fit different sizes with css

--- client/src/components/FeaturedMovieCard.tsx
+++ client/src/components/FeaturedMovieCard.tsx
@@ -4,32 +4,30 @@ import "../styles/movieCard.css";
 import { Link } from "react-router-dom";
 
 interface MovieCardProps {
-  class_name: string;
   tmdb_id: number;
   title: string;
   img: string;
-  year: number;
   description: string;
   genres: string[];
 }
 
 export default function MovieCard(props: MovieCardProps) {
 
-  const { tmdb_id, title, img, year, description, genres, class_name } = props;
+  const { tmdb_id, title, img, description, genres } = props;
   const movie_detail_url = `/movie/${tmdb_id}`;
   // console.log(`img: ${img}`);
 
   return (
-    <div className={class_name}>
-      <img src={img} alt="movie backdrop" />
+    <div className="feature-movie-card">
+      <img src={props.img} alt="movie backdrop" />
       {/* <div className="colored" /> */}
-      <div className={`card-text ${class_name}`}>
-        <div className={`title ${class_name}`}>{title}</div>
-        <p className={`${class_name} desc`}>{description}</p>
-        <div className={`bottom-text ${class_name}`}>
-          <div className={`movie-info ${class_name}`}>
-            <div>{year}</div>
-            <div className={`genres ${class_name}`}>
+      <div className="feature-movie-card-text">
+        <div className="title">{title}</div>
+        <p className="desc">{description}</p>
+        <div className="bottom-text">
+          <div className="movie-info">
+            <div>2010</div>
+            <div className="genres">
               {genres.map((genre, i) => {
                 if (i === genres.length - 1) {
                   return <span key={i}>{genre}</span>;
@@ -41,13 +39,11 @@ export default function MovieCard(props: MovieCardProps) {
             <div className="runtime">1 h 30 min</div>
           </div>
           {/* //TODO: Map styles to above genres */}
-          <div className="btn-container">
-          <button className={`details-btn ${class_name}`}>
+          <button className="details">
             <Link to={movie_detail_url}>
               More details
             </Link>
           </button>
-          </div>
         </div>
       </div>
     </div>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FeaturedMovieCard from "../components/FeaturedMovieCard";
 import MovieCard from "../components/MovieCard";
 
 import "../styles/home.css";
@@ -15,11 +16,21 @@ function Home() {
       </div>
       <div>
         {/* Feature movie cards go here */}
-        <MovieCard
+        <FeaturedMovieCard
+          // class_name="feature-movie-card"
           title="Inception"
           tmdb_id={27205}
-          img="https://media.wired.com/photos/59341d3286599a3834f7cf6c/master/pass/inception_paris_660.jpg"
+          img="https://image.tmdb.org/t/p/original/s3TBrRGB1iav7gFOCNx3H31MoES.jpg"
           description="Dom Cobb (Leonardo DiCaprio) is a thief with the rare ability to enter people's dreams and steal their secrets from their subconscious. His skill has made him a hot commodity in the world of corporate espionage but has also cost him everything he loves. Cobb gets a chance at redemption when he is of…"
+          genres={["Action", "Adventure", "Sci-Fi"]}
+        />
+        <MovieCard
+          class_name="movie-card"
+          title="Inception"
+          tmdb_id={27205}
+          year={2010}
+          img="https://media.wired.com/photos/59341d3286599a3834f7cf6c/master/pass/inception_paris_660.jpg"
+          description="Dom Cobb (Leonardo DiCaprio) is a thief with the rare ability to enter people's dreams and steal their secrets from their subconscious"
           genres={["Action", "Adventure", "Sci-Fi"]}
         />
       </div>
